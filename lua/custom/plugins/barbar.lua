bd470e66038eaec1457eff27003880aff4ec0d84
RafaelXokito <Rafael_Pereira_2000@hotmail.com>
2023-09-30 18:57:22 +0100

    tab changes
    Nothing here

--- lua/custom/plugins/barbar.lua
+++ lua/custom/plugins/barbar.lua
@@ -1,7 +1,14 @@
 local keymap = vim.keymap
 
-keymap.set({"n", "v"}, "<leader>t", '<Cmd>BufferNext<CR>')
---keymap.set({"n", "v"}, "<leader>tp", '<Cmd>BufferPrevious<CR>')
+keymap.set({"n", "v"}, "<leader>tn", '<Cmd>BufferNext<CR>')
+keymap.set({"n", "v"}, "<leader>tp", '<Cmd>BufferPrevious<CR>')
+keymap.set({"n", "v"}, "<leader>tx", "<Cmd>BufferClose<CR>")
+
+-- Alt + Tab next buffer
+keymap.set({"n", "v"}, "<A-Tab>", '<Cmd>BufferNext<CR>')
+-- Alt + w close current buffer
+keymap.set({"n", "v"}, "∑", '<Cmd>BufferClose<CR>')
+
 
 return {
   "romgrk/barbar.nvim",
